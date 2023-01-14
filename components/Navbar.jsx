86225d23b232a868c7d5d630a5b2e01c165889cf
3 Moon <3moon@3s-MacBook-Pro-2.local>
2023-01-14 01:49:34 -0500

    replace img tag in Navbar for Image tag

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -51,7 +51,7 @@ const Navbar = () => {
         <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
             <Link href='/'>
             <Image 
-                //    src="/../public/assets/t-logo01.webp" 
+                
                    src={logo}
                    alt="letter T logo"
                    width='55' 
