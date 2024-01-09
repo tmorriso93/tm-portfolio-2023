dd6b7b77fac13091f1c17acd508d012eb811a2c0
3 Moon <3moon@3s-mbp-2.home>
2024-01-09 12:02:21 -0500

    update landrover description

--- pages/landrover.jsx
+++ pages/landrover.jsx
@@ -13,7 +13,7 @@ const landrover = () => {
             <Image className='absolute z-1' layout='fill' objectFit='cover' src={landroverImg} alt='/' />
             <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
                 <h2 className='py-2'>LANDROVER CAR DEALERSHIP</h2>
-                <h3 className=''>WORDPRESS | POLICARO LANDROVER CAR DEALERSHIP</h3>
+                <h3 className=''>D2C CMS | POLICARO LANDROVER CAR DEALERSHIP</h3>
             </div>
         </div>
 
@@ -22,9 +22,11 @@ const landrover = () => {
                 <p>Project</p>
                 <h2>Overview</h2>
                 <p>
-                I am the sole developer for Policaro Landrover car dealership's website. Within the Policaro Group, I manage eight websites for various car dealerships in Ontario, Canada. This specific website is constructed using the WordPress CMS.
-                 I collaborate with the marketing and graphics team to ensure the maintenance, updates, and modifications meet the needs of our customers in the province.
-                 Daily, I add new web pages and incorporate sales and monthly initiatives onto the site.
+                I am the sole developer for Policaro Land Rover's car dealership website. This Land Rover website was built using the D2C CMS system, similar to WordPress, which allows users to develop complex websites using a content management system and code.
+
+                Within the Policaro Group, I manage eight websites for various car dealerships in Ontario, Canada. I collaborate with the marketing and graphics team to ensure that the maintenance, updates, and modifications meet the needs of our customers in the province.
+
+                Every day, I add new web pages and integrate sales and monthly initiatives into the site. The site is live and is utilized by thousands of Ontarians every month.
                 </p>
                 <button className='px-8 py-2 mt-4 mr-8'><a href="https://www.landroverwaterloo.com/" target='_blank' rel="noreferrer" >Live Site</a></button>
             </div>
@@ -32,7 +34,7 @@ const landrover = () => {
                 <div className='p-2'>
                     <p className='text-center font-bold pb-2'>Technologies</p>
                     <div className='grid grid-cols-3 md:grid-cols-1'>
-                        <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> WORDPRESS</p>
+                        <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> D2C CMS</p>
                         <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> JAVASCRIPT</p>
                         <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> HTML</p>
                         <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> CSS</p>
