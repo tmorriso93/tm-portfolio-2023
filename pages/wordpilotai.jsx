f8a301af82ad6b9bf7d8ceec4515a9fbd9ebabfe
3 Moon <tmorriso93@gmail.com>
2025-07-03 12:08:41 -0400

    remove char in wordpilotai file

--- pages/wordpilotai.jsx
+++ pages/wordpilotai.jsx
@@ -36,7 +36,7 @@ const bmw = () => {
                 </p>
                 <button className='px-8 py-2 mt-4 mr-8'><a href="https://wordpilot-ai-fdpa.vercel.app/" target="_blank" rel="noreferrer" >Live Site</a></button>
                 <button className='px-8 py-2 mt-4 mr-8'><a href="https://github.com/tmorriso93/wordpilot-ai" target="_blank" rel="noreferrer" >Code</a></button>
-                t
+                
             </div>
             <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded'>
                 <div className='p-2'>
